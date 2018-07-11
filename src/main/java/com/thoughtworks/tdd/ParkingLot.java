602b49d08d3e96f97de40defddbbe3eac2026875
XIECI <cisnoc.xie@oocl.com>
2018-07-11 17:22:40 +0800

    pass should_parking_successfully_when_parking_lot_is_not_full(),should_fail_park_when_parking_lot_is_full(),should_get_the_right_car_when_unpark_given_park_receipt()

---
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.tdd;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParkingLot {
+    private int size;
+    private Map<Receipt, Car> carList;
+
+    public ParkingLot(int size) {
+        this.size = size;
+        carList = new HashMap<Receipt, Car>();
+    }
+
+    public Receipt park(Car car) throws ParkingFullException {
+        if (carList.size() == size) {
+            throw new ParkingFullException();
+        }
+        Receipt receipt = new Receipt();
+        carList.put(receipt, car);
+        return receipt;
+    }
+
+    public Car unpark(Receipt receipt) {
+        return carList.remove(receipt);
+    }
+}
---
+++ src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
@@ -0,0 +1,46 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParkingLotTest {
+    @Test
+    public void should_parking_successfully_when_parking_lot_is_not_full() {
+        ParkingLot parkingLot = new ParkingLot(1);
+
+        try {
+            parkingLot.park(new Car());
+        } catch (ParkingFullException e) {
+            fail("should park successfully");
+        }
+    }
+
+    @Test
+    public void should_fail_park_when_parking_lot_is_full() {
+        ParkingLot parkingLot = new ParkingLot(0);
+
+        try {
+            parkingLot.park(new Car());
+            fail("should park successfully");
+        } catch (ParkingFullException e) {
+
+        }
+    }
+
+    @Test
+    public void should_get_the_right_car_when_unpark_given_park_receipt() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car theCar = new Car();
+        Receipt receipt = null;
+        try {
+            receipt = parkingLot.park(theCar);
+        } catch (Exception e) {
+            fail("should park successfully");
+        }
+
+        assertThat(parkingLot.unpark(receipt), is(theCar));
+    }
+}
